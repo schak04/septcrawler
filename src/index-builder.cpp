c5c0bbc6ea957569ef5156f7194bde70ae3c8c60
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-07-09 10:04:02 +0530

    wip(index-builder): readFromDoc function

--- src/index-builder.cpp
+++ src/index-builder.cpp
@@ -9,8 +9,40 @@
  * */
 
 #include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-void readFromDocs();
+std::vector<std::string> readFromDoc(std::string dirName, std::string fileName) {
+    std::string fileToRead = dirName + "/" + fileName;
+    std::ifstream f(fileToRead);
+
+    if (!f.is_open()) {
+        std::cerr << "Error reading " + fileToRead + "\n";
+        return {};
+    }
+
+    std::vector<std::string> linesRead;
+    std::string s;
+    while (std::getline(f, s)) {
+        linesRead.push_back(s);
+    }
+
+    f.close();
+
+    return linesRead;
+}
+
 void normalizeText();
 void tokenizeText();
 void buildPostingLists();
+
+int main() {
+    std::vector<std::string> readLines = readFromDoc("dummy-data", "doc1.txt");
+
+    for (int i = 0; i < readLines.size(); i++) {
+        std::cout << readLines[i] << "\n";
+    }
+
+    return 0;
+}
