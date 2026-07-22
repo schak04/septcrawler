acf31233d53b1fe1da34ffbee69e4a46cf42e79e
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-07-22 11:37:13 +0530

    feat(query-processor): normalize and tokenize dummy queries for now

---
+++ src/query-processor.cpp
@@ -0,0 +1,51 @@
+/*
+ * TODO: use a dummy query for now, actual user queries to be used once the API layer is integrated
+ * the work here is fairly simple:
+ * query -> comes in here -> gets processed (normalized and tokenized) -> is sent to the retrieval engine
+ * */
+
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// #define QUERY "GNU Debugger"
+#define QUERY "Some query related to something educational with %#* () special characters"
+
+std::string normalizeQuery(const std::string& rawQuery) {
+    std::string normalizedQuery = rawQuery;
+
+    std::transform(normalizedQuery.begin(), normalizedQuery.end(), normalizedQuery.begin(), [](unsigned char c) { return std::tolower(c); });
+
+    normalizedQuery.erase(std::remove_if(normalizedQuery.begin(), normalizedQuery.end(), [](unsigned char c) { return std::ispunct(c); }), normalizedQuery.end());
+
+    return normalizedQuery;
+}
+
+std::vector<std::string> tokenizeQuery(const std::string& normalizedQuery) {
+    std::vector<std::string> tokenizedQuery;
+
+    std::stringstream ss(normalizedQuery);
+    std::string token;
+    while (ss >> token) {
+        tokenizedQuery.push_back(token);
+    }
+
+    return tokenizedQuery;
+}
+
+// TODO: pass the tokenizedQuery to the retrieval engine
+
+int main() {
+    std::string normalizedQuery = normalizeQuery(QUERY);
+    std::vector<std::string> tokenizedQuery = tokenizeQuery(normalizedQuery);
+
+    std::cout << QUERY << " -> " << normalizedQuery << '\n';
+    std::cout << "Query tokens: " << '\n';
+    for (const std::string& token : tokenizedQuery) {
+        std::cout << token << '\n';
+    }
+
+    return 0;
+}
