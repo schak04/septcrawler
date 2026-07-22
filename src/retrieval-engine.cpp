6c58e3287a289dc9a96d9d869a95831c0fff6f12
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-07-22 23:27:00 +0530

    wip(retrieval): start implementing retrieval engine

---
+++ src/retrieval-engine.cpp
@@ -0,0 +1,12 @@
+/*
+ * TODO: receive normalized and tokenized queries from the query processor
+ * look up postings from the storage layer (will use dummy postings until I finally implement the storage layer)
+ * generate candidate docs with some lightweight stats for the ranker
+ * send it to the ranker
+ * */
+
+int main() {
+    // ...
+
+    return 0;
+}
