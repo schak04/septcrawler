e46af26bb5d736c88a178f489a001171e7b0784c
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-07-29 10:30:40 +0530

    feat(ranker): implement baseline ranking

--- headers/core-components/ranker.hpp
+++ headers/core-components/ranker.hpp
@@ -1,3 +1,10 @@
 #pragma once
+#include <vector>
+
+#include "../data-structures/candidate-doc.hpp"
+
+int calcScore(const CandidateDocument& cd);
+
+std::vector<CandidateDocument> rankCandidateDocuments(const std::vector<CandidateDocument>& candidateDocs);
 
 int runRanker();
--- src/ranker.cpp
+++ src/ranker.cpp
@@ -7,11 +7,32 @@
 
 #include "../headers/core-components/ranker.hpp"
 
+#include <algorithm>
 #include <iostream>
+#include <vector>
+
+#include "../headers/core-components/retrieval-engine.hpp"
+#include "../headers/data-structures/candidate-doc.hpp"
+
+int calcScore(const CandidateDocument& cd) { return cd.matchedTermsCount; }
+
+std::vector<CandidateDocument> rankCandidateDocuments(const std::vector<CandidateDocument>& unrankedCandidateDocs) {
+    std::vector<CandidateDocument> rankedCandidateDocs = unrankedCandidateDocs;
+
+    std::sort(rankedCandidateDocs.begin(), rankedCandidateDocs.end(), [](const CandidateDocument& a, const CandidateDocument& b) { return calcScore(a) > calcScore(b); });
+
+    return rankedCandidateDocs;
+}
 
 int runRanker() {
     std::cout << "\nRUNNING RANKER...\n";
-    std::cout << "yet to be implemented...\n";
+
+    std::vector<CandidateDocument> unranked = generateCandidateDocuments();
+    std::vector<CandidateDocument> ranked = rankCandidateDocuments(unranked);
+
+    for (const auto& rcd : ranked) {
+        std::cout << "docId: " << rcd.docId << " | matched terms count: " << rcd.matchedTermsCount << '\n';
+    }
 
     return 0;
 }
