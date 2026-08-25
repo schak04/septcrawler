42e05e89788eb61f4ab3674b9cfdcfcab538056e
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-08-25 10:20:38 +0530

    refactor: make port var lowercase

--- internal/api/server.go
+++ internal/api/server.go
@@ -6,11 +6,11 @@ import (
 	"net/http"
 )
 
-var PORT = ":8080"
+var port = ":8080"
 
 type Server struct{}
 
 func (server *Server) Start() {
-	fmt.Println("Server listening on port", PORT)
-	log.Fatal(http.ListenAndServe(PORT, nil))
+	fmt.Println("Server listening on port", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
