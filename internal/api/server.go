330b85621db04ba753a37b5a752b7bd7908f8eba
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-08-25 20:41:41 +0530

    feat(api): return search response as JSON

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,14 +11,23 @@ const port = ":8080"
 
 type Server struct{}
 
+type Response struct {
+	Query string `json:"query"`
+}
+
 func searchHandler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query().Get("q")
+
 	if query == "" {
 		http.Error(w, "missing query", http.StatusBadRequest)
 		return
 	}
 
-	fmt.Println("Query:", query)
+	res := Response{Query: query}
+	encoder := json.NewEncoder(w) // future ref: https://pkg.go.dev/encoding/json#Encoder.Encode
+
+	w.Header().Set("Content-Type", "application/json") // future ref: https://pkg.go.dev/net/http#Header.Set
+	encoder.Encode(res)
 
 	// TODO: connect api to core
 	// slice of Candidate Documents
