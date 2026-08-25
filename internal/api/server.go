0a6d8fd6f0eb122b65e46dd6d780ca206ffd2b1d
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-08-25 19:45:34 +0530

    feat(api): add search endpoint

--- internal/api/server.go
+++ internal/api/server.go
@@ -6,11 +6,31 @@ import (
 	"net/http"
 )
 
-var port = ":8080"
+const port = ":8080"
 
 type Server struct{}
 
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("q")
+	if query == "" {
+		http.Error(w, "missing query", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("Query:", query)
+
+	// TODO: connect api to core
+	// slice of Candidate Documents
+	// SendSearchQueryAndReceiveResultsFromSearchEngine()
+}
+
+func (server *Server) RegisterRoutes() {
+	http.HandleFunc("/search", searchHandler)
+}
+
 func (server *Server) Start() {
+	server.RegisterRoutes()
+
 	fmt.Println("Server listening on port", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
