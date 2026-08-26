12c6f08d611940288bff0e116510105922d529e6
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-08-26 20:43:12 +0530

    feat(bridge): add C-compatible API header

---
+++ core/include/bridge/c-api.h
@@ -0,0 +1,28 @@
+// C-compatible API exposed to Go via cgo
+// Declarations only; implementation will go in a file called bridge.cpp
+
+#ifndef SEPTCRAWLER_BRIDGE_C_API_H
+#define SEPTCRAWLER_BRIDGE_C_API_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    int doc_id;
+    int matched_terms_count;
+} CandidateDocumentC;
+
+typedef struct {
+    CandidateDocumentC* documents;
+    int count;
+} SearchResponseC;
+
+SearchResponseC search_query(const char* query);
+void free_search_response(SearchResponseC response);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
