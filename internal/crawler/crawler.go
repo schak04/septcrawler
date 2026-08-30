7b3c4f43cd35070bf2b8d7a43ec81d941c4cbb7b
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-08-30 13:58:18 +0530

    fix(crawler): close response body on all return paths

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -8,16 +8,15 @@ import (
 
 func Crawl(url string) (string, error) {
 	res, err := http.Get(url)
-
 	if err != nil {
 		return "", err
 	}
+	defer res.Body.Close()
 
 	if res.StatusCode >= 400 && res.StatusCode < 600 {
 		return "", errors.New("Some error occurred")
 	}
 
-	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
 
 	if err != nil {
